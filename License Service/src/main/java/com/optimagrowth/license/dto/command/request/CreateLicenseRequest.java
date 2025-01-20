376562eb7af6d58e957d32c31b732d5fc414f1a0
allan-elong <allan.elong@bjft.ca>
2025-01-20 11:42:17 +0100

    Update - Adding param size alert in the create license DTO

--- License Service/src/main/java/com/optimagrowth/license/dto/command/request/CreateLicenseRequest.java
+++ License Service/src/main/java/com/optimagrowth/license/dto/command/request/CreateLicenseRequest.java
@@ -5,17 +5,21 @@ import com.optimagrowth.license.validation.annotation.LicenseIdNotAlreadyExists;
 import com.optimagrowth.license.validation.annotation.ProductNameNotAlreadyExists;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record CreateLicenseRequest(
         @LicenseIdNotAlreadyExists
         @NotBlank(message = "{message.license.id.blank}")
+        @Size(min = 3, max = 50, message = "{message.license.id.size.alert}")
         String licenseId,
 
         @NotBlank(message = "{The license description cannot be blank}")
+        @Size(min = 3, max = 50, message = "{message.license.description.size.alert}")
         String description,
 
         @ProductNameNotAlreadyExists
         @NotBlank(message = "{message.license.product.name.blank}")
+        @Size(min = 3, max = 50, message = "{message.license.product.name.size.alert}")
         String productName,
 
         @NotBlank(message = "{message.license.type.blank}")
