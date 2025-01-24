c66bec7bb30c2c41e9ab5b0ca6a88936a15fe54b
allan-elong <allan.elong@bjft.ca>
2025-01-24 17:44:29 +0100

    Update - Resolving commit warnings

--- common-library/api/src/main/java/com/optimagrowth/commonlibrary/api/exception/GlobalExceptionHandler.java
+++ common-library/api/src/main/java/com/optimagrowth/commonlibrary/api/exception/GlobalExceptionHandler.java
@@ -28,7 +28,6 @@ import org.springframework.web.multipart.support.MissingServletRequestPartExcept
 import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import javax.annotation.Nullable;
 import java.nio.file.AccessDeniedException;
 import java.util.*;
 
