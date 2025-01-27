e8e5991117c6c06938bf115dae663cbabd9767b0
allan-elong <allan.elong@bjft.ca>
2025-01-27 10:01:12 +0100

    Update - Handling Json patch exception

--- License Service/src/main/java/com/optimagrowth/license/exception/LicenseExceptionHandler.java
+++ License Service/src/main/java/com/optimagrowth/license/exception/LicenseExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.optimagrowth.license.exception;
 
+import com.github.fge.jsonpatch.JsonPatchException;
 import com.mongodb.MongoException;
 import com.optimagrowth.commonlibrary.api.component.ProblemBuilder;
 import com.optimagrowth.license.exception.runtine.LicenseNotFoundException;
@@ -16,15 +17,45 @@ public class LicenseExceptionHandler {
 
     private final ProblemBuilder problemBuilder;
 
+    /**
+     * Handles {@link LicenseNotFoundException} by returning a ProblemDetail response.
+     *
+     * @param exception The LicenseNotFoundException that was thrown.
+     * @return A ProblemDetail object containing information about the error.
+     *         The HTTP status code is set to NOT_FOUND (404).
+     */
     @ExceptionHandler(LicenseNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ProblemDetail handleLicenseNotFoundException(LicenseNotFoundException exception) {
-        return problemBuilder.buildGenericProblemDetail(exception.getMessage(), HttpStatus.NOT_FOUND);
+
+        return problemBuilder.buildGenericProblemDetail(exception.getLocalizedMessage(), HttpStatus.NOT_FOUND);
     }
 
+    /**
+     * Handles {@link MongoException} by returning a ProblemDetail response.
+     *
+     * @param exception The MongoException that was thrown.
+     * @return A ProblemDetail object containing information about the error.
+     *         The HTTP status code is set to SERVICE_UNAVAILABLE (503).
+     */
     @ExceptionHandler(MongoException.class)
     @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
     public ProblemDetail handleMongoException(MongoException exception) {
-        return problemBuilder.buildRuntimeProblemDetail(exception.getMessage(), HttpStatus.SERVICE_UNAVAILABLE);
+
+        return problemBuilder.buildRuntimeProblemDetail(exception.getLocalizedMessage(), HttpStatus.SERVICE_UNAVAILABLE);
+    }
+
+    /**
+     * Handles {@link JsonPatchException} by returning a ProblemDetail response.
+     *
+     * @param exception The JsonPatchException that was thrown.
+     * @return A ProblemDetail object containing information about the error.
+     *         The HTTP status code is set to BAD_REQUEST (400).
+     */
+    @ExceptionHandler(JsonPatchException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ProblemDetail handleJsonPatchException(JsonPatchException exception) {
+
+        return problemBuilder.buildGenericProblemDetail(exception.getLocalizedMessage(), HttpStatus.BAD_REQUEST);
     }
 }
--- License Service/src/main/java/com/optimagrowth/license/exception/runtine/LicenseNotFoundException.java
+++ License Service/src/main/java/com/optimagrowth/license/exception/runtine/LicenseNotFoundException.java
@@ -1,10 +1,13 @@
 package com.optimagrowth.license.exception.runtine;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import java.io.FileNotFoundException;
 
+@Data
+@EqualsAndHashCode(callSuper = true)
 public class LicenseNotFoundException extends FileNotFoundException {
 
-    public LicenseNotFoundException(String message) {
-        super(message);
-    }
+    private String message;
 }
