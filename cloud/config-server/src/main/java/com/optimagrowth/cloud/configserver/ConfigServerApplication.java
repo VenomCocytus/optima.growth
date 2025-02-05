eb0b419a4836ad4fbcbb7901cfe8d495218ef32d
allan-elong <allan.elong@bjft.ca>
2025-02-05 17:07:04 +0100

    Update - Adding some config for the licensing service, removing secret and testing them

--- cloud/config-server/src/main/java/com/optimagrowth/cloud/configserver/ConfigServerApplication.java
+++ cloud/config-server/src/main/java/com/optimagrowth/cloud/configserver/ConfigServerApplication.java
@@ -2,9 +2,9 @@ package com.optimagrowth.cloud.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
-@RefreshScope
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
